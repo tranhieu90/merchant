f034b3f5cb0ab8a354537ed6b6262ba697ea00ca
tranhieu90 <it.tranhieu@gmail.com>
2025-06-13 23:05:12 +0700

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-SRNRFE2E.mjs";import"./chunk-K5ZFBF47.mjs";import"./chunk-XEZNFAMA.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
---
+++ server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{m as o,n as r,o as n}from"./chunk-K5ZFBF47.mjs";import{qc as e}from"./chunk-XEZNFAMA.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
